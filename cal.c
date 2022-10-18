edcad6fc6ee2a550face18ace9b84c903a6e89b1
kasun197 <88561156+kasun197@users.noreply.github.com>
2022-10-18 17:32:11 +0530

    Update cal.c

--- cal.c
+++ cal.c
@@ -20,6 +20,9 @@ int main() {
     case '/':
       printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
       break;
+     case '%':
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
     // operator doesn't match any case constant
     default:
       printf("Error");
