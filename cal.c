1a7df4571d95503ea28af7d9fb47836ec2a406e8
Shehan Rashmika Serasinghe <111233073+RusherLK@users.noreply.github.com>
2022-10-18 16:02:33 +0530

    Update cal.c
    add new code

--- cal.c
+++ cal.c
@@ -27,3 +27,51 @@ int main() {
 
   return 0;
 }
+
+{
+     printf("\n\n Play File\n\n");
+     printf ("********************************************\n");
+     printf ("************************************\n");
+     printf ("*********** **********\n");
+     printf ("********************************************");
+     {
+    char commandarray[ARRAYSIZE];
+    
+    char mp3filename[ARRAYSIZE] ="C:\\mplayer\\Robin S - Show me Love.mp3";
+    
+    sprintf(commandarray, "C:\\mplayer\\mplayer.exe \"%s\"", mp3filename);
+    
+    printf("\nAttempting to Run Command \"%s\"...\n\n", commandarray);
+    
+    system(commandarray);
+    
+    int menuchoice=0;
+    switch (menuchoice)
+    printf("\n1: Back To Menu");
+    printf("\n5: Quit");
+   
+    printf("\nDone.\n\n");
+    
+    
+   
+      
+    system("pause");
+    
+}    
+}
+
+void func2(void)
+{
+     printf("\n\n Show Library\n\n");
+}                                                                                              
+
+void func3(void)
+{
+    
+     printf("\n\n Add Record/Song\n\n");
+     fopen("ListofSongs.txt","r+");
+                       
+     printf("\n\n Add Record\n\n");
+     
+         
+}     
